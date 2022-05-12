a4766a20fdefcdb2b6de00aa14ddd16dc9e3b6aa
Phil Olalere <olalerephilip@gmail.com>
2022-05-12 16:01:56 +0100

    removed some unused variables

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React,{useState} from 'react';
-// import Navbar from './components/includes/Navbar';
-// import Home from './pages/Auth/Login';
-// import Register from './pages/Auth/Login';
-// import Login from './pages/Auth/Login';
-import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
-// import  "./styles/styles.css";
+import {BrowserRouter as Router} from "react-router-dom";
 import AuthRoute from './navigator/AuthRoutes';
 function App() {
-  const [isLoggedIn, setIsLoggedin] = useState(false)
+  // const [isLoggedIn, setIsLoggedin] = useState(false)
   return (
     
     <Router>
-        {/* {!isLoggedIn?
-          <AuthRoute/> */}
-        {/* : */}
+        
 
         <AuthRoute/>
-        {/* } */}
+        
     </Router>
       
     
--- src/navigator/AuthRoutes.js
+++ src/navigator/AuthRoutes.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Home from '../pages/Home';
 import Register from '../pages/Auth/Register';
 import Login from '../pages/Auth/Login';
-import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import  "../styles/styles.css";
-import NavBarComponent from '../components/includes/NavBarComponent';
 function AuthRoute() {
   return (
     
@@ -12,7 +11,6 @@ function AuthRoute() {
         <div className="">
                 <Routes>
                 
-                    {/* <Route path = "/" element={<Home/>}/> */}
                     <Route path = "/login" element={<Login/>}/>
                     <Route path = "/register" element={<Register/>}/>
                     <Route path = "/home" element={<Home/>}/>
