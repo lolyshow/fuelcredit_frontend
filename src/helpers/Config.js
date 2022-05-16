cbfa972e68399a9cfb15da9dfbc645a88ed4c2fe
Phil Olalere <olalerephilip@gmail.com>
2022-05-16 14:15:08 +0100

    added corsanywhere

--- src/helpers/Config.js
+++ src/helpers/Config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://fuelcredit-env.eba-2kmydur3.us-east-1.elasticbeanstalk.com/";
+const BASE_URL = "https://cors-anywhere.herokuapp.com/http://fuelcredit-env.eba-2kmydur3.us-east-1.elasticbeanstalk.com/";
 const Config = {
   base_url: BASE_URL,
 };
